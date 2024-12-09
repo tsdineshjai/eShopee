bb0d20a4cee3a5204fe81136759b8df0c973ea49
D JAI <tsdineshjai@gmail.com>
2024-12-09 16:58:48 +0800

    misc updates

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function Title({ text1, text2 }) {
 	return (
-		<div className=" border flex items-center justify-center w-full gap-2 mt-5">
+		<div className="  flex items-center justify-center w-full gap-2 mt-5">
 			<p className="text-green-600">{text1}</p>
 			<p>{text2}</p>
 			<p className="h-[2px] w-8 bg-red-900"></p>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import useShopContext from "../hooks/useShopContext";
 import React from "react";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 function Product() {
 	const { products, currency } = useShopContext();
@@ -12,88 +13,127 @@ function Product() {
 
 	React.useEffect(() => {
 		const item = products.find((product) => product._id == productId);
-
-		setItemInfo(item);
-		setCenterImage(item.image[0]);
+		if (item) {
+			setItemInfo(item);
+			setCenterImage(item.image[0]);
+		}
 	}, [products, productId]);
 
-	console.log(itemInfo);
-
 	return itemInfo ? (
-		<div className="flex flex-row items-center justify-center gap-3 transition-all ease-linear delay-0  gap-y-5 px-5 border-2 border-gray-700">
-			{/* vertical images */}
-			<section className="flex flex-col gap-3">
-				{itemInfo.image.map((image, index) => (
-					<img
-						src={image}
-						alt="image"
-						key={index}
-						className="w-[125px] cursor-pointer hover:scale-[1.05] transition-transform ease-in-out duration-300"
-						onClick={() => setCenterImage(image)}
-					/>
-				))}
-			</section>
-
-			{/* central image */}
-			<div className="flex w-[50%] justify-self-start">
-				<img src={centerImage} alt="image" className="object-contain" />
-			</div>
-
-			{/* product Info */}
-			<section className=" flex flex-col justify-evenly  ml-3 w-[55%] pt-10   ">
-				<p className="text-[16px] font-semibold">{itemInfo.name}</p>
-
-				{/* star grouping */}
-
-				<div className="flex gap-1 mt-2 mb-2 pr-2 ">
-					<img src={assets.star_icon} alt="rating star" className="h-4" />
-					<img src={assets.star_icon} alt="rating star" className="h-4" />
-					<img src={assets.star_icon} alt="rating star" className="h-4" />
-					<img src={assets.star_icon} alt="rating star" className="h-4" />
-					<img src={assets.star_dull_icon} alt="rating star" className="h-4" />
-					<span className="px-2 font-light h-4 flex items-center justify-center">
-						(73)
-					</span>
-				</div>
-
-				<p className="font-semibold mt-6">
-					{itemInfo.price} <span className="font-light pl-1">{currency}</span>
-				</p>
-
-				<div className="font-normal mt-6 text-[15px]">
-					{itemInfo.description}
-				</div>
-
-				<section className="mt-10 mb-10 flex flex-col gap-3">
-					<p>Select Size</p>
-					<div className="flex flex-row gap-2">
-						{itemInfo.sizes.map((size, index) => (
-							<p
+		<div className=" pt-10 gap-3 transition-opacity ease-in duration-500 opacity-100 ">
+			<div className="flex flex-col gap-12 sm:gap-14 sm:flex-row justify-center items-start">
+				{/*-------------- product images----------- */}
+				<div className="flex-1 flex flex-col sm:flex-row gap-3">
+					<div className="  flex gap-2 sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-between sm:w-[25%] w-full">
+						{itemInfo.image.map((image, index) => (
+							<img
+								src={image}
+								alt="image"
 								key={index}
-								className={` cursor-pointer border-2 p-1 px-3 text-[14px] border-gray-600 ${
-									selectedSize == size ? "bg-gray-900 text-white" : ""
-								} `}
-								onClick={() => setSelectedSize(size)}
-							>
-								{size}
-							</p>
+								className="w-[25%] sm:w-full flex-shrink-0 cursor-pointer hover:scale-[1.05] transition-transform ease-in-out duration-300"
+								onClick={() => setCenterImage(image)}
+							/>
 						))}
 					</div>
-				</section>
-
-				<button className="bg-black mb-10 flex items-center justify-center w-fit text-white px-3 py-2 mt-10  rounded-sm hover:scale-105 transition-transform duration-300 ease-in-out">
-					ADD TO CART
-				</button>
-
-				<div className="text-[15px] font-light ">
-					<hr />
-					<p>100% Original Product.</p>
-					<p>Cash on delivery is available on this product</p>
-					<p>Easy return and exchange policy within 7 days</p>
+					{/* central image */}
+					<div className="flex w-full  ">
+						<img src={centerImage} alt="image" className="w-full h-auto" />
+					</div>
 				</div>
-			</section>
+
+				{/*------------------- product Info------------- */}
+				<section className=" flex flex-1 flex-col justify-normal pr-20 pt-10 ">
+					<p className="text-[16px] font-semibold">{itemInfo.name}</p>
+					{/* star grouping */}
+					<div className="flex gap-1 mt-2 mb-2 pr-2 ">
+						<img src={assets.star_icon} alt="rating star" className="h-3" />
+						<img src={assets.star_icon} alt="rating star" className="h-3" />
+						<img src={assets.star_icon} alt="rating star" className="h-3" />
+						<img src={assets.star_icon} alt="rating star" className="h-3" />
+						<img
+							src={assets.star_dull_icon}
+							alt="rating star"
+							className="h-3"
+						/>
+						<span className="px-2 font-light h-3 text-[14px] flex items-center justify-center">
+							(73)
+						</span>
+					</div>
+
+					<p className="font-normal mt-2  p-0  ">
+						{itemInfo.price} <span className="text-[13px]">{currency}</span>
+					</p>
+
+					<div className="font-normal mt-3 text-[14px] text-gray-500 w-58 md:w-4/5">
+						{itemInfo.description}
+					</div>
+
+					<section className="mt-6 flex flex-col gap-3 mb-3">
+						<p>Select Size</p>
+						<div className="flex flex-row gap-2">
+							{itemInfo.sizes.map((size, index) => (
+								<p
+									key={index}
+									className={` cursor-pointer border-2 p-1 px-3 text-[14px] border-gray-600 ${
+										selectedSize == size ? "bg-gray-900 text-white" : ""
+									} `}
+									onClick={() => setSelectedSize(size)}
+								>
+									{size}
+								</p>
+							))}
+						</div>
+					</section>
+
+					<button className="bg-black mt-2 mb-5  active:bg-gray-700  flex items-center justify-center w-fit text-white px-3 py-2  rounded-sm hover:scale-105 transition-transform duration-300 ease-in-out">
+						ADD TO CART
+					</button>
+
+					<div className="text-[14px] font-light text-gray-500 ">
+						<hr />
+						<p className="pt-3">100% Original Product.</p>
+						<p>Cash on delivery is available on this product</p>
+						<p>Easy return and exchange policy within 7 days</p>
+					</div>
+				</section>
+			</div>
+			{/* product reviews and description */}
+
+			<div className="mt-10">
+				<div className="flex flex-row">
+					<b className="text-sm px-3 py-2 border-[2px] border-gray-200">
+						Description
+					</b>
+					<p className="text-sm font-normal px-3 py-2 border-2 border-gray-200">
+						Reviews (33)
+					</p>
+				</div>
+
+				<div className="flex flex-col gap-4 border px-6 py-6 text-[13px] text-gray-500">
+					<p>
+						E-commerce (Electronic Commerce) refers to the buying and selling of
+						goods and services, or the transmission of funds or data, over an
+						electronic network, primarily the internet. It allows individuals
+						and businesses to conduct transactions online without the need for
+						physical interaction
+					</p>
+					<p>
+						E-commerce (Electronic Commerce) refers to the buying and selling of
+						goods and services, or the transmission of funds or data, over an
+						electronic network, primarily the internet. It allows individuals
+						and businesses to conduct transactions online without the need for
+						physical interaction
+					</p>
+				</div>
+			</div>
+			<RelatedProducts
+				category={itemInfo.category}
+				subCategory={itemInfo.subCategory}
+			/>
 		</div>
-	) : null;
+	) : (
+		<div className="opacity-0"></div>
+	);
 }
 
 export default Product;
